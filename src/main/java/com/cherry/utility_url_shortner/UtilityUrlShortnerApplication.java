e7796e2430e17e57a34191496d0137b8ee602e07
cherry <vishnu.rathore786@gmail.com>
2024-06-11 23:42:31 +0530

    initial commit

---
+++ src/main/java/com/cherry/utility_url_shortner/UtilityUrlShortnerApplication.java
@@ -0,0 +1,13 @@
+package com.cherry.utility_url_shortner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilityUrlShortnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilityUrlShortnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cherry/utility_url_shortner/UtilityUrlShortnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cherry.utility_url_shortner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilityUrlShortnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
